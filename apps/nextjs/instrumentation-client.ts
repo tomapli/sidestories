116de5faed7afa9cfd2170e9e12d4403fb2be755
tpkowastaken <protitom@gmail.com>
2026-06-13 00:22:42 +0200

    Reapply "cookies done"
    This reverts commit d6ac97045f64f90b8ed299d523fd6fd963730056.

--- apps/nextjs/instrumentation-client.ts
+++ apps/nextjs/instrumentation-client.ts
@@ -4,6 +4,7 @@ posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN!, {
   api_host: "/ingest",
   ui_host: "https://eu.posthog.com",
   defaults: "2026-01-30",
+  cookieless_mode: "on_reject",
   capture_exceptions: true,
   debug: process.env.NODE_ENV === "development",
 });
