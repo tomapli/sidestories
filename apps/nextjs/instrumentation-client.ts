36728537d6590d0c181355b313db973c2bb3bb4a
tpkowastaken <protitom@gmail.com>
2026-06-13 00:30:22 +0200

    fix?

--- apps/nextjs/instrumentation-client.ts
+++ apps/nextjs/instrumentation-client.ts
@@ -1,9 +1,11 @@
 import posthog from "posthog-js";
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN!, {
+import { env } from "~/env";
+
+posthog.init(env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN, {
   api_host: "/ingest",
   ui_host: "https://eu.posthog.com",
   defaults: "2026-01-30",
   capture_exceptions: true,
-  debug: process.env.NODE_ENV === "development",
+  debug: env.NODE_ENV === "development",
 });
