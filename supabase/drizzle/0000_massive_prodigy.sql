e22794580a47bbdcc1e38325b593aef19a703ae3
tpkowastaken <protitom@gmail.com>
2026-06-22 11:28:59 +0200

    drizzle working good

---
+++ supabase/drizzle/0000_massive_prodigy.sql
@@ -0,0 +1,21 @@
+CREATE SCHEMA "auth";
+--> statement-breakpoint
+CREATE TABLE "auth"."users" (
+	"id" uuid PRIMARY KEY NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "post" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"title" varchar(256) NOT NULL,
+	"content" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"user_id" uuid PRIMARY KEY NOT NULL,
+	"admin_since" timestamp with time zone,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "users" ADD CONSTRAINT "users_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "auth"."users"("id") ON DELETE cascade ON UPDATE no action;
